7790c5c1ce28ce328525b6379d5773b540ec763f
Cn_Nullptr <justitacsl@outlook.com>
2019-12-10 22:08:17 +0800

    first commit

---
+++ src/main/java/cn/sau/sauoh/SauohApplication.java
@@ -0,0 +1,13 @@
+package cn.sau.sauoh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SauohApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SauohApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/sau/sauoh/SauohApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.sau.sauoh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SauohApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
