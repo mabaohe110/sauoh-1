e281059fd9c3d38ab84ec9771531ef0293261721
Cn_Nullptr <justitacsl@outlook.com>
2019-12-15 20:18:57 +0800

    mybatis config

--- src/main/java/cn/sau/sauoh/SauohApplication.java
+++ src/main/java/cn/sau/sauoh/SauohApplication.java
@@ -1,9 +1,11 @@
 package cn.sau.sauoh;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("cn.sau.sauoh.repository")
 public class SauohApplication {
 
 	public static void main(String[] args) {
