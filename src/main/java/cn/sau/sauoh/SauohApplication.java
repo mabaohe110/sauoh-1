cd4e8f0485c5b0f117831b33fb438c083adc7a75
Cn_Nullptr <justitacsl@outlook.com>
2019-12-16 00:24:47 +0800

    代码规范

--- src/main/java/cn/sau/sauoh/SauohApplication.java
+++ src/main/java/cn/sau/sauoh/SauohApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author nullptr
+ */
 @SpringBootApplication
 @MapperScan("cn.sau.sauoh.repository")
 public class SauohApplication {
