8dbe8132455de9e12807154cb75f383b383ad2ea
Cn_Nullptr <justitacsl@outlook.com>
2019-12-21 11:42:56 +0800

    添加新的user表字段，对应修改实体类和Mapper

--- src/main/java/cn/sau/sauoh/entity/User.java
+++ src/main/java/cn/sau/sauoh/entity/User.java
@@ -1,12 +1,22 @@
 package cn.sau.sauoh.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private Integer id;
     private String username;
     private String password;
+    private Timestamp createTime;
+    private String checkCode;
 
     public User(String username, String password) {
         this.username = username;
