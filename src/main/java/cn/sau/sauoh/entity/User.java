aed24acc08a18c2a754fb6ea113e96b6fa11f6b1
Cn_Nullptr <justitacsl@outlook.com>
2019-12-24 21:47:27 +0800

    RESTful

--- src/main/java/cn/sau/sauoh/entity/User.java
+++ src/main/java/cn/sau/sauoh/entity/User.java
@@ -1,5 +1,8 @@
 package cn.sau.sauoh.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,8 +18,11 @@ public class User {
     private Integer id;
     private String username;
     private String email;
+    @JsonIgnore
     private String password;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private Timestamp createTime;
+    @JsonIgnore
     private String checkCode;
 
     public User(String username, String password){
