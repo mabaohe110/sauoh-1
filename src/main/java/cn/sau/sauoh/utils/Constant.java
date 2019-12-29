77bf4e85b6af82983a34891e7d6863ea640fa8e6
Cn_Nullptr <justitacsl@outlook.com>
2019-12-29 15:12:45 +0800

    添加常量

--- src/main/java/cn/sau/sauoh/utils/Constant.java
+++ src/main/java/cn/sau/sauoh/utils/Constant.java
@@ -2,12 +2,14 @@ package cn.sau.sauoh.utils;
 
 /**
  * 常量
- * 
+ *
  * @author chenshun
  * @email sunlightcs@gmail.com
  * @date 2016年11月15日 下午1:23:52
  */
 public class Constant {
 
+    public static final String ASC = "ASC";
+    public static final String DESC = "DESC";
 
 }
